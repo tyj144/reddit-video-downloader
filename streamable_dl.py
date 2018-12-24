7f0b0694edb4453dffc42f3637a2513f6e96a0f1
Tyler Jiang <tylerj81@gmail.com>
2018-12-24 18:19:30 -0500

    Completed streamable downloader

---
+++ streamable_dl.py
@@ -0,0 +1,45 @@
+from get_html import get_html_from_url
+import bs4
+from video_dl import download_file
+import sys
+import re, string
+
+def main():
+    if (len(sys.argv) == 2):
+        print('''
+     _                                  _     _                _ _
+ ___| |_ _ __ ___  __ _ _ __ ___   __ _| |__ | | ___        __| | |
+/ __| __| '__/ _ \/ _` | '_ ` _ \ / _` | '_ \| |/ _ \_____ / _` | |
+\__ \ |_| | |  __/ (_| | | | | | | (_| | |_) | |  __/_____| (_| | |
+|___/\__|_|  \___|\__,_|_| |_| |_|\__,_|_.__/|_|\___|      \__,_|_|
+        ''')
+        url = sys.argv[1]
+        download_from_url(url)
+    else:
+        print('Usage: http.py <url>')
+
+def get_name_from_url(url):
+    name = ''
+    for i in range(len(url) - 1, 0, -1):
+        if url[i] == '/':
+            return re.sub(r'\W+', '', name)
+        else:
+            name = url[i] + name
+    return re.sub(r'\W+', '', url)
+
+
+def download_from_url(url):
+    html = get_html_from_url(url)
+
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    video_tag = soup.find('video')
+
+    src = 'https:' + video_tag.attrs['src']
+
+    name = get_name_from_url(url)
+    print('name', name)
+
+    download_file('evfrx', src)
+
+if __name__ == "__main__":
+    main()
---
+++ video_dl.py
@@ -0,0 +1,11 @@
+import requests
+
+def download_file(name, url):
+    name = 'videos/' + name + ".mp4"
+    print("Downloading to '%s' from '%s'" % (name, url))
+    r = requests.get(url)
+    with open(name, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=255): 
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+        print("Done")
